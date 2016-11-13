5af5b767d78ecb9af99f2accd775b0b44ba455f6
Matteo Gabriele <matteo@blocklevel.nl>
2016-11-13 23:22:20 +0100

    feat(label) add label property to the translation-tool
    now is possible to decide which name to display in the component's buttons

--- src/component/translationTool/index.js
+++ src/component/translationTool/index.js
@@ -1,17 +1,25 @@
 import template from './index.html'
+import { log } from '../../utils'
 import { SET_FORCE_TRANSLATION } from '../../store/module/events'
 import './index.scss'
 
 export default {
   name: 'translation-tool',
 
-  template,
-
-  mounted () {
-    this.$store.dispatch(SET_FORCE_TRANSLATION, true)
+  props: {
+    label: {
+      type: String,
+      default: 'name'
+    }
   },
 
+  template,
+
   beforeDestroy () {
+    if (!this.visibility) {
+      return
+    }
+
     this.$store.dispatch(SET_FORCE_TRANSLATION, false)
   },
 
@@ -28,11 +36,22 @@ export default {
   },
 
   methods: {
+    getLabel (language) {
+      const label = language[this.label]
+
+      if (!label) {
+        log(`"${this.label}" doesn't exist in the language with code "${language.code}"`, 'warn')
+        return language.code
+      }
+
+      return label
+    },
     isActive (code) {
       return this.$store.getters.currentLanguage.code === code
     },
     toggle () {
       this.visibility = !this.visibility
+      this.$store.dispatch(SET_FORCE_TRANSLATION, this.visibility)
     }
   }
 }
