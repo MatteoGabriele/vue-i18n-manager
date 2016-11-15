b1b38f98ece319b57dfd109989c2c0807e869be7
Matteo Gabriele <matteo@blocklevel.nl>
2016-11-15 12:30:33 +0100

    fix(loaders) add missing loaders

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -8,5 +8,13 @@ module.exports = [
       presets: ['es2015', 'stage-2']
     },
     exclude: /node_modules/
+  },
+  {
+    test: /\.html$/,
+    loader: 'html-loader'
+  },
+  {
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader']
   }
 ]
