bdda2f3552114b4ead003a1e2220e5d6cd2ff37a
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-17 23:41:39 +0100

    refactor(proxy):  remove http call from translation.js

--- src/proxy/translation.js
+++ src/proxy/translation.js
@@ -1,5 +1,3 @@
-import { warn } from '../utils'
-
 /**
  * Retrieves the current language translation
  * @param  {Object}  language
@@ -7,27 +5,9 @@ import { warn } from '../utils'
  * @return {Promise}
  */
 export const getTranslation = (state, language) => {
-  // const requestURL = `${state.path}/${language.translateTo}.json`
-  //
-  // const request = new Request(requestURL, {
-  //   method: 'GET',
-  //   mode: 'cors',
-  //   headers: new Headers({
-  //     'Content-Type': 'application/json'
-  //   })
-  // })
-  //
-  // return fetch(request, { credentials: 'same-origin' }).then(response => {
-  //   if (!response.ok) {
-  //     if (response.status === 404) {
-  //       warn('Translation error. Check if the file exists and the url is correct')
-  //     } else {
-  //       warn(`${response.statusText} for ${requestURL}`)
-  //     }
-  //
-  //     return {}
-  //   }
-  //
-  //   return response.json()
-  // })
+  return new Promise((resolve) => {
+    resolve({
+      message: 'hello'
+    })
+  })
 }
